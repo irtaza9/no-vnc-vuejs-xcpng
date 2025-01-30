5a83b600f9fc2eaf8c0c8786ed5718c636145ee9
Irtaza Hussain <irtaza.hussain@nayatel.com>
2025-01-30 15:21:57 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>noVNC Demo</h1>
+    <VncViewer :url="vncUrl" :is-console-available="true" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import VncViewer from './components/VncViewer.vue'
+
+// Replace with your VNC server URL
+const vncUrl = ref(new URL('wss://domain/api/consoles/id'))
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
